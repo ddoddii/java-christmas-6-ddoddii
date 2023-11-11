b5039e7ae769853178a5cf0e688a7f288240cdb2
Soeun Uhm <uhmturks@yonsei.ac.kr>
2023-11-12 01:27:29 +0900

    feat : 주문 메뉴 출력

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -1,10 +1,26 @@
 package christmas.view;
 
-public class OutputView {
+import static christmas.view.ViewMessage.ORDERED_MENU;
 
-    public void displayEventMessage(int date){
+import christmas.util.Parser;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class OutputView {
+    private final String QUANTITY_SUFFIX = "개";
+
+    public void displayEventMessage(int date) {
         String formattedMessage = String.format(ViewMessage.EVENT.getMessage(), date);
         System.out.println(formattedMessage);
     }
+
+    public void displayOrderedMenu(Map<String, Integer> parsedMenu) {
+        String orders = parsedMenu.entrySet().stream()
+                .map(entry -> entry.getKey() + " " + entry.getValue() + QUANTITY_SUFFIX)
+                .collect(Collectors.joining("\n"));
+        System.out.println(ORDERED_MENU.getMessage());
+        System.out.println(orders);
+    }
     
+
 }
