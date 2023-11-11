100345c582bb977e7c9931025510a4691529dc9a
Soeun Uhm <uhmturks@yonsei.ac.kr>
2023-11-12 01:31:49 +0900

    feat : 할인 전 총주문 금액 출력

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -1,13 +1,16 @@
 package christmas.view;
 
+import static christmas.view.ViewMessage.AMOUNT_BEFORE_DISCOUNT;
 import static christmas.view.ViewMessage.ORDERED_MENU;
 
 import christmas.util.Parser;
+import java.util.Locale;
 import java.util.Map;
 import java.util.stream.Collectors;
 
 public class OutputView {
     private final String QUANTITY_SUFFIX = "개";
+    private final String MONEY_SUFFIX = "원";
 
     public void displayEventMessage(int date) {
         String formattedMessage = String.format(ViewMessage.EVENT.getMessage(), date);
@@ -21,6 +24,16 @@ public class OutputView {
         System.out.println(ORDERED_MENU.getMessage());
         System.out.println(orders);
     }
+
+    public void displayTotalOrderAmount(int amount) {
+        String formattedAmount = formatMoney(amount);
+        System.out.println(AMOUNT_BEFORE_DISCOUNT.getMessage());
+        System.out.println(formattedAmount + MONEY_SUFFIX);
+    }
     
+    private String formatMoney(int amount) {
+        return String.format(Locale.US, "%,d", amount);
+    }
+
 
 }
