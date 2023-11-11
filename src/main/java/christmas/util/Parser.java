747f76f00ad1e66328f68ae8181e6e7a95f335aa
Soeun Uhm <uhmturks@yonsei.ac.kr>
2023-11-11 19:41:12 +0900

    feat : menu input 을 파싱하는 기능
    - Menu input 을 파싱해서 Map 형태로 반환

--- src/main/java/christmas/util/Parser.java
+++ src/main/java/christmas/util/Parser.java
@@ -1,14 +1,29 @@
 package christmas.util;
 
 import christmas.exception.date.DateNotIntegerException;
-import christmas.exception.date.DateRangeException;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Parser {
-    public static int parseDate(String input){
-        try{
+    public static int parseDate(String input) {
+        try {
             return Integer.parseInt(input);
-        } catch (NumberFormatException e){
+        } catch (NumberFormatException e) {
             throw new DateNotIntegerException();
         }
     }
+
+    /*
+    input 으로 들어온 메뉴 파싱
+     */
+    public static Map<String, Integer> parseMenuCount(String input) {
+        return Arrays.stream(input.split(","))
+                .map(item -> item.split("-"))
+                .collect(Collectors.toMap(
+                        parts -> parts[0],
+                        parts -> Integer.parseInt(parts[1])
+                ));
+    }
+
 }
