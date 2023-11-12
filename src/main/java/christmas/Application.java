29d7d631de3849ac2134e43c4250a5f3d6adfcef
Soeun Uhm <uhmturks@yonsei.ac.kr>
2023-11-12 19:26:57 +0900

    feat : 어플리케이션 내에서 controller 호출

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -1,7 +1,15 @@
 package christmas;
 
+import christmas.controller.PromotionController;
+import christmas.service.PromotionService;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        PromotionController promotionController = new PromotionController(
+                new PromotionService()
+        );
+
+        promotionController.run();
+
     }
 }
