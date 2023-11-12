627b5da0e3cc8084d5473865dbc931b4e4853f28
Soeun Uhm <uhmturks@yonsei.ac.kr>
2023-11-12 15:41:24 +0900

    chore : 변수명 변경

--- src/main/java/christmas/view/ViewMessage.java
+++ src/main/java/christmas/view/ViewMessage.java
@@ -8,7 +8,7 @@ public enum ViewMessage {
     EVENT("12월 %d에 우테코 식당에서 받을 이벤트 혜택 미리 보기!"),
     ORDERED_MENU("<주문 메뉴>"),
     AMOUNT_BEFORE_DISCOUNT("<할인 전 총주문 금액>"),
-    GIFT_EVENT("<증정 메뉴>"),
+    GIFT_MENU("<증정 메뉴>"),
     PROMOTION("<혜택 내역>"),
     TOTAL_PROMOTION_AMOUNT("<총혜택 금액>"),
     EXPECTED_AMOUNT("<할인 후 예상 결제 금액>"),
